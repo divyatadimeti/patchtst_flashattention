7e46609769672e43e482e020d2eed6776f32736d
Khushi Desai <khushipdesai@gmail.com>
2024-04-01 14:05:56 -0400

    Add code to run.py to create the dataloaders and models based on config type specified

--- patchtst_vanilla.py
+++ patchtst_vanilla.py
@@ -3,3 +3,24 @@ from transformers import (
     PatchTSTForPrediction
 )
 
+def get_vanilla_patchtst(params):
+    config = PatchTSTConfig(do_mask_input=False,
+                            context_length=params["context_length"],
+                            patch_length=params["patch_length"],
+                            num_input_channels=params["num_input_channels"],
+                            patch_stride=params["patch_length"],
+                            prediction_length=params["prediction_length"],
+                            d_model=params["d_model"],
+                            num_attention_heads=params["num_attention_heads"],
+                            num_hidden_layers=params["num_hidden_layers"],
+                            ffn_dim=512,
+                            dropout=0.2,
+                            head_dropout=0.2,
+                            pooling_type=None,
+                            channel_attention=False,
+                            scaling="std",
+                            loss="mse",
+                            pre_norm=True,
+                            norm_type="batchnorm")
+    model = PatchTSTForPrediction(config=config)
+    return model
--- run.py
+++ run.py
@@ -3,23 +3,35 @@ import argparse
 import wandb
 
 from dataloader import get_ETT_dataloaders
+from patchtst_vanilla import get_vanilla_patchtst
 
 parser = argparse.ArgumentParser(description="Training PatchTST vanilla and FlashAttention2 models for benchmarking and profiling")
 parser.add_argument("-c", "--config", type=str, default="./config.yaml")
 args = parser.parse_args()
 
 config = yaml.safe_load(open(args.config))
+model_config = config["model"]
+train_config = config["training"]
+data_config = config["data"]
+log_config = config["logging"]
 
-run = wandb.init(project=config["logging"]["wandb_project"], 
-           entity=config["logging"]["wandb_entity"],
-           name=config["logging"]["wandb_run_name"])
+run = wandb.init(project=log_config["wandb_project"], 
+           entity=log_config["wandb_entity"],
+           name=log_config["wandb_run_name"])
 assert run is wandb.run
 
-train_dataloader, valid_dataloader, test_dataloader = get_ETT_dataloaders(config["data"], 
-                                                                          config["model"]["context_length"], 
-                                                                          config["model"]["forecast_horizon"],
-                                                                          config["training"]["batch_size"],
-                                                                          config["training"]["num_workers"])
+dataset = train_config["dataset"]
 
+if dataset == "ETTh1" or dataset == "ETTm1":
+    train_dataloader, valid_dataloader, test_dataloader = get_ETT_dataloaders(data_config[dataset], 
+                                                                          model_config["context_length"], 
+                                                                          model_config["forecast_horizon"],
+                                                                          train_config["batch_size"],
+                                                                          train_config["num_workers"])
+else:
+    pass
 
-
+if model_config["model_type"] == "vanilla":
+    model = get_vanilla_patchtst(model_config)
+else:
+    pass
