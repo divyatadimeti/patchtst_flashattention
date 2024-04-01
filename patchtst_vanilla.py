83351f97a233d25585c4aa8d3542a0c0be045374
Khushi Desai <khushipdesai@gmail.com>
2024-04-01 18:38:30 -0400

    Integrated trainer with pytorch lightning

--- patchtst_vanilla.py
+++ patchtst_vanilla.py
@@ -1,18 +1,24 @@
+import torch
+import pytorch_lightning as pl
+import torch.nn.functional as F
+
 from transformers import (
     PatchTSTConfig,
     PatchTSTForPrediction
 )
 
-def get_vanilla_patchtst(params):
-    config = PatchTSTConfig(do_mask_input=False,
-                            context_length=params["context_length"],
-                            patch_length=params["patch_length"],
-                            num_input_channels=params["num_input_channels"],
-                            patch_stride=params["patch_length"],
-                            prediction_length=params["prediction_length"],
-                            d_model=params["d_model"],
-                            num_attention_heads=params["num_attention_heads"],
-                            num_hidden_layers=params["num_hidden_layers"],
+class PatchTSTVanilla(pl.LightningModule):
+    def __init__(self, model_config):
+        super().__init__()
+        config = PatchTSTConfig(do_mask_input=False,
+                            context_length=model_config["context_length"],
+                            patch_length=model_config["patch_length"],
+                            num_input_channels=model_config["num_input_channels"],
+                            patch_stride=model_config["patch_length"],
+                            prediction_length=model_config["prediction_length"],
+                            d_model=model_config["d_model"],
+                            num_attention_heads=model_config["num_attention_heads"],
+                            num_hidden_layers=model_config["num_hidden_layers"],
                             ffn_dim=512,
                             dropout=0.2,
                             head_dropout=0.2,
@@ -22,5 +28,24 @@ def get_vanilla_patchtst(params):
                             loss="mse",
                             pre_norm=True,
                             norm_type="batchnorm")
-    model = PatchTSTForPrediction(config=config)
-    return model
+        self.model = PatchTSTForPrediction(config=config)
+
+    def forward(self, x):
+        return self.model(x)
+
+    def training_step(self, batch, batch_idx):
+        x, y = batch
+        y_hat = self(x)
+        loss = F.mse_loss(y_hat, y)
+        self.log('train_loss', loss)
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        x, y = batch
+        y_hat = self(x)
+        val_loss = F.mse_loss(y_hat, y)
+        self.log('val_loss', val_loss)
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.parameters(), lr=0.001)
+        return optimizer
--- run.py
+++ run.py
@@ -1,38 +1,72 @@
 import yaml
 import argparse
 import wandb
+import pytorch_lightning as pl
 
 from dataloader import get_ETT_dataloaders
-from patchtst_vanilla import get_vanilla_patchtst
+from patchtst_vanilla import PatchTSTVanilla
+from pytorch_lightning.loggers import WandbLogger
+from pytorch_lightning.callbacks import EarlyStopping, ModelCheckpoint, LearningRateMonitor
 
-parser = argparse.ArgumentParser(description="Training PatchTST vanilla and FlashAttention2 models for benchmarking and profiling")
-parser.add_argument("-c", "--config", type=str, default="./config.yaml")
-args = parser.parse_args()
+def main(args):
+    # Load the configs for model, training, and logging parameters
+    with open(args.config, "r") as file:
+        config = yaml.safe_load(file)
+        
+    model_config = config["model"]
+    train_config = config["training"]
+    data_config = config["data"]
+    log_config = config["logging"]
 
-config = yaml.safe_load(open(args.config))
-model_config = config["model"]
-train_config = config["training"]
-data_config = config["data"]
-log_config = config["logging"]
+    # Set up wandb logging and PyTorch Lightning logger
+    run = wandb.init(project=log_config["wandb_project"], 
+            entity=log_config["wandb_entity"],
+            name=log_config["wandb_run_name"])
+    assert run is wandb.run
 
-run = wandb.init(project=log_config["wandb_project"], 
-           entity=log_config["wandb_entity"],
-           name=log_config["wandb_run_name"])
-assert run is wandb.run
+    logger = WandbLogger(project=config["logging"]["wandb_project"],
+                         entity=config["logging"]["wandb_entity"],
+                         name=config["logging"]["wandb_run_name"])
 
-dataset = train_config["dataset"]
+    # Load dataset and dataloaders depending on the dataset chosen for training
+    dataset = train_config["dataset"]
 
-if dataset == "ETTh1" or dataset == "ETTm1":
-    train_dataloader, valid_dataloader, test_dataloader = get_ETT_dataloaders(data_config[dataset], 
-                                                                          model_config["context_length"], 
-                                                                          model_config["forecast_horizon"],
-                                                                          train_config["batch_size"],
-                                                                          train_config["num_workers"])
-else:
-    pass
+    if dataset == "ETTh1" or dataset == "ETTm1":
+        train_dataloader, val_dataloader, test_dataloader = get_ETT_dataloaders(data_config[dataset], 
+                                                                            model_config["context_length"], 
+                                                                            model_config["forecast_horizon"],
+                                                                            train_config["batch_size"],
+                                                                            train_config["num_workers"])
+    else:
+        pass
 
-if model_config["model_type"] == "vanilla":
-    model = get_vanilla_patchtst(model_config)
-else:
-    pass
+    # Load the appropriate model (either Vanilla or FlashAttention2)
+    if model_config["model_type"] == "vanilla":
+        model = PatchTSTVanilla(model_config)
+    else:
+        pass
 
+    # Set up callbacks for early stopping, model checkpointing and learning rate scheduling
+    early_stop_callback = EarlyStopping(monitor="val_loss", patience=train_config["patience"])
+    checkpoint_callback = ModelCheckpoint(dirpath=log_config["checkpoint_path"], 
+                                              monitor="val_loss", 
+                                              save_top_k=1, 
+                                              mode="min")
+    lr_monitor = LearningRateMonitor(logging_interval='step')
+
+    # Set up the trainer
+    trainer = pl.Trainer(
+        max_epochs=config["training"]["epochs"],
+        logger=logger,
+        callbacks=[early_stop_callback, checkpoint_callback, lr_monitor],
+        gpus=1,
+    )
+
+    trainer.fit(model, train_dataloader, val_dataloader)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Training PatchTST vanilla and FlashAttention2 models for benchmarking and profiling")
+    parser.add_argument("-c", "--config", type=str, default="./config.yaml")
+    args = parser.parse_args()
+
+    main(args)
