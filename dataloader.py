88fc5537d77fa9939250c166e916feb2703fc076
Khushi Desai <khushipdesai@gmail.com>
2024-04-01 12:41:08 -0400

    Create dataloader and model set up for ETTh1

---
+++ dataloader.py
@@ -0,0 +1,106 @@
+import torch
+from torch.utils.data import DataLoader
+import pandas as pd
+
+from tsfm_public.toolkit.dataset import ForecastDFDataset
+from tsfm_public.toolkit.time_series_preprocessor import TimeSeriesPreprocessor
+from tsfm_public.toolkit.util import select_by_index
+
+def get_ETT_datasets(data_path, context_length, forecast_horizon):
+    timestamp_column = "date"
+    id_columns = []
+    forecast_columns = ["HUFL", "HULL", "MUFL", "MULL", "LUFL", "LULL", "OT"]
+    train_start_index = None
+    train_end_index = 12 * 30 * 24
+
+    valid_start_index = 12 * 30 * 24 - context_length
+    valid_end_index = 12 * 30 * 24 + 4 * 30 * 24
+
+    test_start_index = 12 * 30 * 24 + 4 * 30 * 24 - context_length
+    test_end_index = 12 * 30 * 24 + 8 * 30 * 24
+
+    data = pd.read_csv(
+        data_path,
+        parse_dates=[timestamp_column],
+    )
+
+    train_data = select_by_index(
+        data,
+        id_columns=id_columns,
+        start_index=train_start_index,
+        end_index=train_end_index,
+    )
+    valid_data = select_by_index(
+        data,
+        id_columns=id_columns,
+        start_index=valid_start_index,
+        end_index=valid_end_index,
+    )
+    test_data = select_by_index(
+        data,
+        id_columns=id_columns,
+        start_index=test_start_index,
+        end_index=test_end_index,
+    )
+
+    tsp = TimeSeriesPreprocessor(
+        timestamp_column=timestamp_column,
+        id_columns=id_columns,
+        target_columns=forecast_columns,
+        scaling=True,
+    )
+    tsp.train(train_data)
+
+    train_dataset = ForecastDFDataset(
+        tsp.preprocess(train_data),
+        id_columns=id_columns,
+        target_columns=forecast_columns,
+        context_length=context_length,
+        prediction_length=forecast_horizon,
+    )
+    valid_dataset = ForecastDFDataset(
+        tsp.preprocess(valid_data),
+        id_columns=id_columns,
+        target_columns=forecast_columns,
+        context_length=context_length,
+        prediction_length=forecast_horizon,
+    )
+    test_dataset = ForecastDFDataset(
+        tsp.preprocess(test_data),
+        id_columns=id_columns,
+        target_columns=forecast_columns,
+        context_length=context_length,
+        prediction_length=forecast_horizon,
+    )
+
+    return train_dataset, valid_dataset, test_dataset
+
+def get_ETT_dataloaders(data_config, 
+                        context_length, 
+                        forecast_horizon,
+                        batch_size=32, 
+                        num_workers=2):
+    data_path = data_config["data_path"]
+    train_dataset, valid_dataset, test_dataset = get_ETT_datasets(data_path, context_length, forecast_horizon)
+    train_dataloader = DataLoader(
+        train_dataset,
+        batch_size=batch_size,
+        shuffle=True,
+        num_workers=num_workers,
+    )
+
+    valid_dataloader = DataLoader(
+        valid_dataset,
+        batch_size=batch_size,
+        shuffle=False,
+        num_workers=num_workers,
+    )
+
+    test_dataloader = DataLoader(
+        test_dataset,
+        batch_size=batch_size,
+        shuffle=False,
+        num_workers=num_workers,
+    )
+    
+    return train_dataloader, valid_dataloader, test_dataloader
--- run.py
+++ run.py
@@ -2,6 +2,8 @@ import yaml
 import argparse
 import wandb
 
+from dataloader import get_ETT_dataloaders
+
 parser = argparse.ArgumentParser(description="Training PatchTST vanilla and FlashAttention2 models for benchmarking and profiling")
 parser.add_argument("-c", "--config", type=str, default="./config.yaml")
 args = parser.parse_args()
@@ -13,6 +15,11 @@ run = wandb.init(project=config["logging"]["wandb_project"],
            name=config["logging"]["wandb_run_name"])
 assert run is wandb.run
 
+train_dataloader, valid_dataloader, test_dataloader = get_ETT_dataloaders(config["data"], 
+                                                                          config["model"]["context_length"], 
+                                                                          config["model"]["forecast_horizon"],
+                                                                          config["training"]["batch_size"],
+                                                                          config["training"]["num_workers"])
 
 
 
