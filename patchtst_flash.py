dc1f133b67dca97d341fb939e91871d73703cd7a
divyatadimeti <46757986+divyatadimeti@users.noreply.github.com>
2024-04-15 23:59:23 -0400

    Fix imports

--- patchtst_flash.py
+++ patchtst_flash.py
@@ -3,7 +3,8 @@ import torch.nn as nn
 import numpy as np
 
 from ast import Tuple
-from pyparsing import Optional
+# from pyparsing import Optional
+from typing import Optional
 from transformers import PatchTSTForPrediction, PatchTSTConfig, PatchTSTAttention
 from flash_attn import flash_attn_func, flash_attn_varlen_func
 from flash_attn.bert_padding import index_first_axis, pad_input, unpad_input
