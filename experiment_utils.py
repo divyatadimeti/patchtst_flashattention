4814e7e9c50b08d8a434e172c63742f9b96bc2b8
Divya Tadimeti <dtadimeti@berkeley.edu>
2024-04-22 16:44:14 -0400

    setting up experiments

---
+++ experiment_utils.py
@@ -0,0 +1,108 @@
+import yaml
+import argparse
+import wandb
+import pytorch_lightning as pl
+
+from dataloader import get_ETT_dataloaders
+from patchtst_model import PatchTST
+from pytorch_lightning.loggers import WandbLogger
+from pytorch_lightning.callbacks import EarlyStopping, ModelCheckpoint, LearningRateMonitor
+
+def patch_sizes_experiment(model_config, train_config, log_config, data_config):
+    patch_sizes = [12, 24, 48, 96, 192]
+    for patch_size in patch_sizes:
+        model_config["patch_length"] = patch_size
+        attn_type = model_config["attn_type"]
+        log_config["wandb_run_name"] = f"patchtst_{attn_type}_patchsize_{patch_size}"
+        driver(model_config, train_config, log_config, data_config)
+
+def batch_sizes_experiment(model_config, train_config, log_config, data_config):
+    batch_sizes = [32, 64, 128, 256, 512]
+    for batch_size in batch_sizes:
+        train_config["batch_size"] = batch_size
+        attn_type = model_config["attn_type"]
+        log_config["wandb_run_name"] = f"patchtst_{attn_type}_batchsize_{batch_size}"
+        driver(model_config, train_config, log_config, data_config)
+
+def dataset_experiment(model_config, train_config, log_config, data_config):
+    datasets = ["ETTh1", "ETTm1", "Weather"]
+    for data in datasets:
+        data_config["dataset"] = data
+        attn_type = model_config["attn_type"]
+        log_config["wandb_run_name"] = f"patchtst_{attn_type}_dataset_{data}"
+        driver(model_config, train_config, log_config, data_config)
+
+def num_workers_experiment(model_config, train_config, log_config, data_config):
+    num_workers = [2, 4, 8, 16]
+    for num in num_workers:
+        train_config["num_workers"] = num
+        attn_type = model_config["attn_type"]
+        log_config["wandb_run_name"] = f"patchtst_{attn_type}_numworkers_{num}"
+        driver(model_config, train_config, log_config, data_config)
+
+
+def driver(model_config, train_config, log_config, data_config):
+     # Set up wandb logging and PyTorch Lightning logger
+    # Set up wandb logging and PyTorch Lightning logger
+    logger = None
+    if log_config["use_wandb"]:
+        run = wandb.init(project=log_config["wandb_project"], 
+                entity=log_config["wandb_entity"],
+                name=log_config["wandb_run_name"])
+        assert run is wandb.run
+
+        wandb.config.update(model_config)
+        wandb.config.update(train_config)
+        wandb.config.update(data_config)
+        wandb.config.update(log_config)
+
+        logger = WandbLogger(project=log_config["wandb_project"],
+                            entity=log_config["wandb_entity"],
+                            name=log_config["wandb_run_name"])
+
+    # Load dataset and dataloaders depending on the dataset chosen for training
+    dataset = data_config["dataset"]
+
+    if dataset == "ETTh1" or dataset == "ETTm1":
+        train_dataloader, val_dataloader, test_dataloader = get_ETT_dataloaders(data_config, 
+                                                                            model_config["context_length"], 
+                                                                            model_config["forecast_horizon"],
+                                                                            train_config["batch_size"],
+                                                                            train_config["num_workers"])
+    else:
+        pass
+
+    # Load the appropriate model (either Vanilla or FlashAttention2)
+    if model_config["attn_type"] == "vanilla":
+        model = PatchTST(model_config)
+    else:
+        model = PatchTST(model_config)
+
+    # Set up callbacks for early stopping, model checkpointing and learning rate scheduling
+    callbacks = []
+    if train_config["early_stopping"]:
+        early_stop_callback = EarlyStopping(monitor="val_loss", patience=train_config["patience"])
+        callbacks.append(early_stop_callback)
+
+    checkpoint_callback = ModelCheckpoint(dirpath=log_config["checkpoint_path"], 
+                                              monitor="val_loss", 
+                                              save_top_k=1, 
+                                              mode="min")
+    lr_monitor = LearningRateMonitor(logging_interval='step')
+    callbacks.append(checkpoint_callback)
+    callbacks.append(lr_monitor)
+
+    # Set up the trainer
+    trainer = pl.Trainer(
+        accelerator="gpu",
+        devices=1,
+        max_epochs=train_config["epochs"],
+        logger=logger,
+        callbacks=callbacks,
+    )
+
+    # Train the model
+    trainer.fit(model, train_dataloader, val_dataloader)
+
+    # Test the model
+    trainer.test(model, dataloaders=test_dataloader)
--- run.py
+++ run.py
@@ -5,87 +5,38 @@ import pytorch_lightning as pl
 
 from dataloader import get_ETT_dataloaders
 from patchtst_model import PatchTST
+from experiment_utils import patch_sizes_experiment, batch_sizes_experiment, dataset_experiment, num_workers_experiment, driver
 from pytorch_lightning.loggers import WandbLogger
 from pytorch_lightning.callbacks import EarlyStopping, ModelCheckpoint, LearningRateMonitor
+
 
 def main(args):
     # Load the configs for model, training, and logging parameters
     with open(args.config, "r") as file:
         config = yaml.safe_load(file)
-        
+    
+    config = config["default_config"]
     model_config = config["model"]
     train_config = config["training"]
     data_config = config["data"]
     log_config = config["logging"]
+    
+    attn_types = ["vanilla", "flash"]
 
-    # Set up wandb logging and PyTorch Lightning logger
-    logger = None
-    if log_config["use_wandb"]:
-        run = wandb.init(project=log_config["wandb_project"], 
-                entity=log_config["wandb_entity"],
-                name=args.run_name)
-        assert run is wandb.run
+    for attn in attn_types:
+        model_config["attn_type"] = attn
+        patch_sizes_experiment(model_config, train_config, data_config, log_config)
+        batch_sizes_experiment(model_config, train_config, data_config, log_config)
+        dataset_experiment(model_config, train_config, data_config, log_config)
+        num_workers_experiment(model_config, train_config, data_config, log_config)
 
-        wandb.config.update(model_config)
-        wandb.config.update(train_config)
-        wandb.config.update(data_config)
-        wandb.config.update(log_config)
 
-        logger = WandbLogger(project=log_config["wandb_project"],
-                            entity=log_config["wandb_entity"],
-                            name=args.run_name)
+    # driver(model_config, train_config, log_config, data_config)
 
-    # Load dataset and dataloaders depending on the dataset chosen for training
-    dataset = train_config["dataset"]
-
-    if dataset == "ETTh1" or dataset == "ETTm1":
-        train_dataloader, val_dataloader, test_dataloader = get_ETT_dataloaders(data_config[dataset], 
-                                                                            model_config["context_length"], 
-                                                                            model_config["forecast_horizon"],
-                                                                            train_config["batch_size"],
-                                                                            train_config["num_workers"])
-    else:
-        pass
-
-    # Load the appropriate model (either Vanilla or FlashAttention2)
-    if model_config["attn_type"] == "vanilla":
-        model = PatchTST(model_config)
-    else:
-        model = PatchTST(model_config)
-
-    # Set up callbacks for early stopping, model checkpointing and learning rate scheduling
-    callbacks = []
-    if train_config["early_stopping"]:
-        early_stop_callback = EarlyStopping(monitor="val_loss", patience=train_config["patience"])
-        callbacks.append(early_stop_callback)
-
-    checkpoint_callback = ModelCheckpoint(dirpath=log_config["checkpoint_path"], 
-                                              monitor="val_loss", 
-                                              save_top_k=1, 
-                                              mode="min")
-    lr_monitor = LearningRateMonitor(logging_interval='step')
-    callbacks.append(checkpoint_callback)
-    callbacks.append(lr_monitor)
-
-    # Set up the trainer
-    trainer = pl.Trainer(
-        accelerator="gpu",
-        devices=1,
-        max_epochs=train_config["epochs"],
-        logger=logger,
-        callbacks=callbacks,
-    )
-
-    # Train the model
-    trainer.fit(model, train_dataloader, val_dataloader)
-
-    # Test the model
-    trainer.test(model, dataloaders=test_dataloader)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Training PatchTST vanilla and FlashAttention2 models for benchmarking and profiling")
     parser.add_argument("-c", "--config", type=str, default="./config.yaml")
-    parser.add_argument("--run_name", type=str, default="project-test")
     args = parser.parse_args()
 
     main(args)
